316aa875a09ebd8b10958ecf6f0a7fb2b2e37d26
Pavel Khaliman <pinnadeee@gmail.com>
2025-08-21 02:48:31 +0300

    add game

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
 
 class EventServiceProvider extends ServiceProvider
@@ -14,6 +15,7 @@ class EventServiceProvider extends ServiceProvider
      */
     protected $listen = [
         Registered::class => [
+            SendEmailVerificationNotification::class,
             \App\Listeners\NotifyTelegramAboutRegistration::class,
         ],
     ];
